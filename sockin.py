e1127d785467beb6935e11a3f4d3ca8939873b0e
cyberslut <114898116+whoiscyberslut@users.noreply.github.com>
2024-05-28 08:56:21 +0200

    Update sockin.py

--- sockin.py
+++ sockin.py
@@ -10,4 +10,14 @@ try:
   print(f"Hostname: {host}")
 except:
   print("An error occurred.")
+
+
+# Using requests module to downoad any web page
+
+import requests
+
+producturl = "http://100daysofdevops.com/"  
   
+res = requests.get(producturl, timeout = 5) # save output to a variable  
+print(res)
+print(res.text) # -> complete web page
