1429a48bc7ce6ad89d5e0fb5e57314dce4e1fc22
cyberslut <114898116+whoiscyberslut@users.noreply.github.com>
2024-07-12 13:43:43 +0300

    Update perfect_date.py

--- perfect_date.py
+++ perfect_date.py
@@ -7,7 +7,7 @@ def perfect_date(day, month, year):
   date = day + '/' + month + '/' + year
   print('The perfect date (DD-MM-YYYY) is: " + date)
 
-date = '2'
+day = '2'
 month = '6'
 year = '2022'
 perfect_date(day, month, year)
