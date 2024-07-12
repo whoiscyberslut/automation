395f3c3a026d9c3be320e37b0f1a0dca969b7eb0
cyberslut <114898116+whoiscyberslut@users.noreply.github.com>
2024-07-12 12:08:18 +0300

    Create sshdispatcher.py

---
+++ sshdispatcher.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+"""
+A SSH-based command dispatch system
+"""
+
+machines = ["10.0.1.40", "10.0.1.50", "10.0.1.51", "10.0.1.60", "10.0.1.80"]
+cmd = "uname"
+
+for machine in machines:
+    try:
+        subprocess.check_call(f"ssh root@{machine} {cmd}", shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to execute command on {machine}: {e}")
+    except Exception as e:
+        print(f"An error occurred with {machine}: {e}")
