bc2d6371213ca0bd2068dea074f4e5c521abc69a
cyberslut <114898116+whoiscyberslut@users.noreply.github.com>
2024-07-12 07:14:59 +0300

    Update sysmonitor.py

--- sysmonitor.py
+++ sysmonitor.py
@@ -209,9 +209,31 @@ if __name__ == '__main__':
   
 #    return res 
   
-  
 #if __name__ == '__main__': 
 #  ping('www.google.com') 
+
+# OR: 
+
+def ping(mysite):
+    myping = ("ping -q -c 1 %s > /dev/null" % mysite)
+    status = os.system(myping)
+    return(status)
+
+with open("sites.txt") as file:
+    mysites = file.readlines()
+
+for site in mysites:
+    mystatus = ping(site.strip())
+    if mystatus == 0:
+   	 print(site.strip() + " is fine")
+    if mystatus != 0:
+   	 print("********************")
+   	 print("vvvvvvvvvvvvvvvvvvvv")
+   	 print("%s is down!" % (site.strip()))
+   	 print("^^^^^^^^^^^^^^^^^^^^")
+   	 print("********************")
+    print("-----------")
+
 
 # Example 8: Fetching the free disk space
 
