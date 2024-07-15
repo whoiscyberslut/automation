7254bd0cdb56d0bce01d088c87a01e8eae0fd897
cyberslut <114898116+whoiscyberslut@users.noreply.github.com>
2024-07-16 00:38:38 +0300

    Update apache_log_parser.py

--- apache_log_parser.py
+++ apache_log_parser.py
@@ -10,55 +10,47 @@ with the total number of bytes transferred to them.
 
 import sys
 
-def dictify_logline(line):
+def parse_log_line(line):
     """
-    Return a dictionary of the pertinent pieces of an apache combined log file.
-    Currently, the only fields we are interested in are remote host and bytes sent,
-    but we are putting status in there just for good measure.
+    Parse a single log line and return the remote host and bytes sent.
     """
-    split_line = line.split()
-    return {
-        'remote_host': split_line[0],
-        'status': split_line[8],
-        'bytes_sent': split_line[9],
-    }
+    parts = line.split()
+    return parts[0], parts[9]
 
 def generate_log_report(logfile):
     """
-    Return a dictionary of format remote_host => [list of bytes sent].
-    This function takes a file object, iterates through all the lines in the file,
-    and generates a report of the total number of bytes transferred to each remote host.
+    Generate a report of total bytes transferred per remote host from the log file.
     """
-    report_dict = {}
+    report = {}
     for line in logfile:
-        line_dict = dictify_logline(line)
+        remote_host, bytes_sent = parse_log_line(line)
         try:
-            bytes_sent = int(line_dict['bytes_sent'])
+            bytes_sent = int(bytes_sent)
         except ValueError:
-            # Ignore lines where bytes_sent is not a valid integer
             continue
-        
-        # Populate report_dict with remote_host as key and list of bytes_sent as value
-        report_dict.setdefault(line_dict['remote_host'], []).append(bytes_sent)
-    
-    return report_dict
+        if remote_host in report:
+            report[remote_host] += bytes_sent
+        else:
+            report[remote_host] = bytes_sent
+    return report
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) != 2:
         print(__doc__)
         sys.exit(1)
-    
+
     infile_name = sys.argv[1]
-    
     try:
         with open(infile_name, 'r') as infile:
             log_report = generate_log_report(infile)
-            print(log_report)
+            for host, total_bytes in log_report.items():
+                print(f"{host}: {total_bytes} bytes")
     except IOError:
-        print(f"You must specify a valid file to parse: {infile_name}")
-        print(__doc__)
+        print(f"Could not read file: {infile_name}")
         sys.exit(1)
 
+if __name__ == "__main__":
+    main()
 
 # Example input:
 
