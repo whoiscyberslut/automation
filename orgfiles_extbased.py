8e631c4b5822e52d468e3eb8039a0e1a15cb056b
cyberslut <114898116+whoiscyberslut@users.noreply.github.com>
2024-07-21 19:36:25 +0200

    Update orgfiles_extbased.py

--- orgfiles_extbased.py
+++ orgfiles_extbased.py
@@ -2,23 +2,30 @@
 
 import os
 import shutil
+import sys
 
 def fileorganise(directory):
-  for element in os.listdir(directory):
-    full_path = os.path.join(directory, element)
-    if os.path.isfile(full_path):
-      # Split the file based on the '.' delimiter and extract the last part
-      parts = element.split(".")
-      file_extension = parts[-1]
-      # Create directory if not exists
-      destdir = os.path.join(directory, file_extension)
-      os.makedirs(destdir, exist_ok=True) # suppresses OSError if the directory already exists
-      # Move the file to the corrensponding directory
-      shutil.move(full_path, destdir)
-  
+    for element in os.listdir(directory):
+        full_path = os.path.join(directory, element)
+        if os.path.isfile(full_path):
+            # Split the file based on the '.' delimiter and extract the last part
+            parts = element.split(".")
+            file_extension = parts[-1]
+            # Create directory if not exists
+            destdir = os.path.join(directory, file_extension)
+            os.makedirs(destdir, exist_ok=True) # suppresses OSError if the directory already exists
+            # Move the file to the corresponding directory
+            shutil.move(full_path, destdir)
+
 if __name__ == '__main__':
-  directory = os.getcwd()
-  # directory = r"Enter your path to run this script"
-  # Example:
-  # directory = r"/home/user/folder"
-  fileorganise(directory)
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <directory>")
+        sys.exit(1)
+    
+    directory = sys.argv[1]
+    
+    if not os.path.isdir(directory):
+        print(f"The specified path {directory} is not a valid directory.")
+        sys.exit(1)
+
+    fileorganise(directory)
